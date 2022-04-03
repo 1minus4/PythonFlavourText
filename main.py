385fb78940651fba0c4bc5e81437f3d3e229c731
1minus4 <76790421+1minus4@users.noreply.github.com>
2022-04-03 16:49:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,15 @@ def MaximumFlava():
 	num1 = int(input("do:"))
 	num2 = 0
 	timeBetw = float(input("seconds between: "))
+	# just empty space, don't worry about it
 	print("")
+	# loop to produce output desired no. of times
 	while num2 < num1:
 		output = pyFlavText.testingLOL()
+		strLenny = len(output)
+		# for debugging purposes
+		print(strLenny) # ( ͡° ͜ʖ ͡°)
 		num2+=1
 		time.sleep(timeBetw)
 
-MaximumFlava()
+MaximumFlava() 
